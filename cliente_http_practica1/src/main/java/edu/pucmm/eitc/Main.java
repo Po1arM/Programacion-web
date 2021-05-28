dba024328d1992edf3a0e9fef0d3b5b9793908d3
Felix Guzman <20180520@ce.pucmm.edu.do>
2021-05-28 12:40:56 -0400

    Creacion del proyecto para la primera practica

---
+++ cliente_http_practica1/src/main/java/edu/pucmm/eitc/Main.java
@@ -0,0 +1,15 @@
+package edu.pucmm.eitc;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Ingrese un URL: ");
+        Scanner scanner = new Scanner(System.in);
+        String url =  scanner.nextLine();
+        //String url = "https://raw.githubusercontent.com/vacax/javalin-demo/master/src/main/resources/publico/formulario.html";
+
+    }
+}
