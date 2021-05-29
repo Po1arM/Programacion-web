3f28e384bb54931fca0ba66b8708d7fd66bef455
Felix Guzman <20180520@ce.pucmm.edu.do>
2021-05-29 16:02:57 -0400

    Puntos del A al E realizados

--- cliente_http_practica1/src/main/java/edu/pucmm/eitc/Main.java
+++ cliente_http_practica1/src/main/java/edu/pucmm/eitc/Main.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -16,7 +17,7 @@ public class Main {
         //System.out.println("Ingrese un URL: ");
         //Scanner scanner = new Scanner(System.in);
         //String url =  scanner.nextLine();
-        String url = "https://acento.com.do/el-financiero/fase-para-trabajadores-del-sector-turismo-otorgara-subsidios-hasta-julio-8949165.html";
+        String url = "https://raw.githubusercontent.com/FGuzman29/Universidad/main/prueba.html?token=ALXFEXZ5FZZZU3FQOTTIKWTAXPDTG";
         Document doc = Jsoup.connect(url).get();
 
         //Cantidad de lineas del recurso
@@ -24,7 +25,7 @@ public class Main {
         System.out.print("La cantidad de lineas en el documento es de: " + lines + " lineas");
 
         //Cantidad de parrafos
-        Elements para = doc.getElementsByTag("p");
+        Elements para = doc.select("p");
         System.out.println("\nLa cantidad de parrafos es de: " + para.size());
 
         //Cantidad de imagenes dentro de parrafos
@@ -33,5 +34,28 @@ public class Main {
             System.out.println("La cantidad de imagenes dentro de parrafos es de: " + img.size());
         }
 
+        Elements postForms = doc.select("form[method$=post]");
+        System.out.println("La cantidad de formularios con metodo POST es de: " + postForms.size());
+
+        Elements getForms = doc.select("form[method$=get]");
+        System.out.println("La cantidad de formularios con metodo GET es de: " + getForms.size()+"\n");
+
+        inputs(postForms);
+        inputs(getForms);
+    }
+
+    public static void inputs(Elements forms){
+        int cont = 1;
+        for (Element form: forms) {
+            if(form.childrenSize() != 0) {
+                System.out.println("Los inputs del formulario " + form.attr("method").toLowerCase() + " #"+ cont+ " son:");
+                for (Element child : form.children()) {
+                    System.out.println("\t"+ child);
+                }
+            }else{
+                System.out.println("El formulario " + form.attr("method").toLowerCase() +" #"+ cont +" no tiene campos input como hijos");
+            }
+            cont++;
+        }
     }
 }
