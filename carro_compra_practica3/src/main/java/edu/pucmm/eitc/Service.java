5f6768de74b0cbc5496270e18d9a07cb1f52a18e
Felix Guzman <20180520@ce.pucmm.edu.do>
2021-06-13 23:01:04 -0400

    Iniciar el server a traves de BootStrap

--- carro_compra_practica3/src/main/java/edu/pucmm/eitc/Service.java
+++ carro_compra_practica3/src/main/java/edu/pucmm/eitc/Service.java
@@ -19,6 +19,8 @@ public class Service {
     private long carrito;
 
     public Service() {
+        BootStrapServices.startDB();
+
         DBService.getInstance().testConnection();
         try {
             BootStrapServices.crearTablas();
